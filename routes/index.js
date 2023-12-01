006e615f256d6c9537e3486a6f3b4842f60c1b91
mainak4ever <mainak4ever@gmail.com>
2023-12-01 21:56:52 +0530

    create pin page created

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,83 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const userModel = require("./users");
+const postModel = require("./posts.js");
+const { default: mongoose } = require("mongoose");
+const passport = require("passport");
+
+const localStrategy = require("passport-local");
+passport.use(new localStrategy(userModel.authenticate()));
+
+// to use email for login instead of username.
+// passport.use(new localStrategy({ usernameField: 'email' }, userModel.authenticate()));
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
+router.get("/login", function (req, res, next) {
+  res.render("login", { title: "Login", error: req.flash("error") });
+});
+router.get("/feed", function (req, res, next) {
+  res.render("feed", { title: "Feed" });
+});
+
+router.get("/create", function (req, res, next) {
+  res.render("create");
+});
+
+router.get("/profile", isLoggedIn, async function (req, res, next) {
+  const user = await userModel.findOne({ username: req.session.passport.user });
+  console.log(user);
+  res.render("profile", { title: "profile", user });
+});
+
+router.post("/register", function (req, res, next) {
+  const { username, email, fullname } = req.body;
+  const userData = new userModel({ username, email, fullname });
+
+  userModel.register(userData, req.body.password).then(function () {
+    passport.authenticate("local")(req, res, function () {
+      res.redirect("/profile");
+    });
+  });
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/profile",
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  function (req, res, next) {}
+);
+
+// router.post(
+//   "/login",
+//   passport.authenticate("local", {
+//     failureRedirect: "/login",
+//     failureMessage: true,
+//   }),
+//   function (req, res) {
+//     res.redirect("/profile");
+//   }
+// );
+
+router.get("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,42 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require("mongoose");
+const plm = require("passport-local-mongoose");
+require("dotenv").config();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const url = process.env.MONGO_URI;
+// console.log(url);
+mongoose.connect(url);
+
+const userSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  dp: {
+    type: String,
+  },
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post", // Assuming 'Post' is another Mongoose model
+    },
+  ],
 });
 
-module.exports = router;
+userSchema.plugin(plm);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
